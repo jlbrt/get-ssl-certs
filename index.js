cdecc46407643e794e0db2d8b381132a89247cc6
Jonas Liebert <mail@jonas-liebert.de>
2021-11-09 17:56:24 +0100

    refactorings + update version to 2.0.0

--- index.js
+++ index.js
@@ -1,65 +1,51 @@
 #! /usr/bin/env node
 
-const https = require("https");
-const chalk = require("chalk");
-const packageJSON = require("./package.json");
-const { program } = require("commander");
+const https = require('https');
+const { stdout } = require('process');
 
-program
-  .version(packageJSON.version)
-  .arguments("<url>")
-  .action(async (url) => {
-    if (url) {
-      const certificateChain = await getCertificateChain(url);
+(async () => {
+  const url = process.argv[2];
 
-      for (let i = 0; i < certificateChain.length; i++) {
-        const certificate = certificateChain[i];
+  if (!url) return;
 
-        let heading;
+  const certificateChain = await getCertificateChain(url);
 
-        if (i === 0) {
-          heading = "# Root Certificate";
-        } else if (i === certificateChain.length - 1) {
-          heading = "# End-user Certificate";
-        } else {
-          heading = "# Intermediate Certificate " + i;
-        }
-        console.log(heading + "\n");
+  let output = '';
 
-        console.log(pemEncode(certificate) + "\n");
+  for (let i = 0; i < certificateChain.length; i++) {
+    const certificate = certificateChain[i];
 
-        console.log(
-          chalk.bold("Common Name: ") + (certificate.subject.CN || "")
-        );
-        console.log(
-          chalk.bold("Organization: ") + (certificate.subject.O || "")
-        );
-        console.log(
-          chalk.bold("Valid From: ") + (certificate.valid_from || "")
-        );
-        console.log(chalk.bold("Valid To: ") + (certificate.valid_to || ""));
-        console.log(
-          chalk.bold("Issuer: ") +
-            (certificate.issuer.CN || "") +
-            ", " +
-            (certificate.issuer.O || "")
-        );
-        console.log(
-          chalk.bold("Serial Number: ") + (certificate.serialNumber || "")
-        );
+    let heading;
 
-        if (i !== certificateChain.length - 1) console.log("\n\n");
-      }
+    if (i === 0) {
+      heading = '# Root Certificate';
+    } else if (i === certificateChain.length - 1) {
+      heading = '# End-user Certificate';
+    } else {
+      heading = '# Intermediate Certificate ' + i;
     }
-  });
+    output += `${heading}\n\n`;
 
-program.parse();
+    output += `${pemEncode(certificate)}\n\n`;
+
+    output += `Common Name: ${certificate.subject.CN || ''}\n`;
+    output += `Organization: ${certificate.subject.O || ''}\n`;
+    output += `Valid From: ${certificate.valid_from || ''}\n`;
+    output += `Valid To: ${certificate.valid_to || ''}\n`;
+    output += `Issuer: ${certificate.issuer.CN || ''}, ${certificate.issuer.O || ''}\n`;
+    output += `Serial Number: ${certificate.serialNumber || ''}\n`;
+
+    if (i !== certificateChain.length - 1) output += '\n\n\n';
+  }
+
+  stdout.write(output + '\n');
+})();
 
 function getCertificateChain(url) {
   return new Promise((resolve, reject) => {
-    if (!url.startsWith("http")) url = `https://${url}`;
+    if (!url.startsWith('http')) url = `https://${url}`;
 
-    const host = "https://" + new URL(url).host; // Includes port if set
+    const host = 'https://' + new URL(url).host; // Includes port if set
 
     https.get(host, { agent: https.Agent({ maxCachedSessions: 0 }) }, (res) => {
       let certificate = res.socket.getPeerCertificate(true);
@@ -79,22 +65,21 @@ function getCertificateChain(url) {
 }
 
 function pemEncode(certificate) {
-  const str = certificate.raw.toString("base64");
+  const certRaw = certificate.raw.toString('base64');
 
-  const ret = [];
+  const chunks = [];
 
-  for (let i = 1; i <= str.length; i++) {
-    ret.push(str[i - 1]);
-    const mod = i % 64;
+  const certRawLength = certRaw.length;
+  while (chunks.length * 64 < certRawLength) {
+    const i = chunks.length * 64;
 
-    if (mod === 0 && i < str.length) {
-      ret.push("\n");
-    }
+    const chunk = certRaw.slice(i, i + 64);
+    chunks.push(chunk);
   }
 
-  const returnString = `-----BEGIN CERTIFICATE-----\n${ret.join(
-    ""
-  )}\n-----END CERTIFICATE-----`;
-
-  return returnString;
+  return (
+    '-----BEGIN CERTIFICATE-----\n' +
+    chunks.join('\n') +
+    '\n-----END CERTIFICATE-----'
+  );
 }
